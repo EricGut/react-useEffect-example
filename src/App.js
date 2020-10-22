fbfc0055bd05faa4fa18b235d1163a877383490c
eric <ericgutierrezinfo@gmail.com>
2020-10-22 20:57:48 +0200

    useEffect example in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './style/App.css';
 
 function App() {
+  // we declarate where the api is
+  const apiUrl = 'https://course-api.netlify.app/api/react-tours-project';
+
+  // we create a use state for save the json api
+  const [tours, setTours] = useState([]);
+
+  // function to fetch the data from the api and use useState to push the data
+  const getTours = async () => {
+    const fetchTours = await fetch(apiUrl);
+    const tours = await fetchTours.json();
+    setTours(tours);
+  }
+
+  /* using useEffect when the page render once and initializing the getTours function 
+    useEffect intializes everytime the page re-render but in this case we use '[]' to initialize just one time.
+    if we remove the '[]' the app would crash.
+  */
+  useEffect(() => {
+    getTours();
+  }, []);
+
+  // the return of the actual object
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div className="container">
+        <h1 className="title">Available tours</h1>
+        <h2 className="tours-info">There are a total of {tours.length} tours</h2>
+        <div className="tours">
+          {/* render the tours from the api */}
+          {
+            tours.map((tour) => {
+              /* desctructuring tour to get all the properties */
+              const { id, name, info, image, price } = tour;
+              return (
+                <div className="card" key={id}>
+                  <div className="card--img">
+                    <img src={image} alt="" />
+                  </div>
+                  <div className="card--info">
+                    <strong className="name">{name}</strong>
+                    <p className="description">{info}</p>
+                    <span className="price">{price}€</span>
+                  </div>
+                </div>
+              );
+            })
+          }
+        </div>
+      </div>
+    </section>
   );
 }
 
